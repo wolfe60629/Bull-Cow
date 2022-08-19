4c6cad7cdf070a5f7772d71f4cecb3591cdaf22c
Jeremy Wolfe <jeremy.wolfe@gm.com>
2022-08-19 15:57:26 -0400

    inital

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {CardModule} from "primeng/card";
+import {InputTextModule} from "primeng/inputtext";
+import {ButtonModule} from "primeng/button";
+import {AccordionModule} from "primeng/accordion";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    AccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
